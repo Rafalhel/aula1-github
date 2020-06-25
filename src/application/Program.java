43c43c7573f4a014be3d40bc352df80a5e5bc678
Rafael Gomes <rafaelgs04@gmail.com>
2020-06-25 16:50:05 -0300

    Acrescentado bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
-
-/**
- *
- * @author Ludoxi
- */
 public class Program {
+    public static void main(String[]args){
+        System.out.println("Bom dia");
+    }
     
 }
